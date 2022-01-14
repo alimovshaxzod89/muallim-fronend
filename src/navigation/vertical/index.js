e4639f739e278ecc704cde14130ac1d4e75b7fb1
Muhammad Samadov <sam.muhammadali@gmail.com>
2022-01-14 15:51:20 +0500

    fixed

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -11,35 +11,35 @@ import views from './views'
 // export default [...dashboard, ...appAndPages, ...uiElements, ...formsAndTables, ...chartsAndMaps, ...others]
 
 
-import admin from './roles/admin'
-import director from './roles/director'
-import manager from './roles/manager'
-import operator from './roles/operator'
-import teacher from './roles/teacher'
-
+// import admin from './roles/admin'
+// import director from './roles/director'
+// import manager from './roles/manager'
+// import operator from './roles/operator'
+// import teacher from './roles/teacher'
+//
 let roleMenu = []
-
-const userData = JSON.parse(localStorage.getItem('userData'))
-if (userData && userData.role) {
-	switch (userData.role) {
-		case 'admin':
-			roleMenu = admin
-			break
-		case 'director':
-			roleMenu = director
-			break
-		case 'manager':
-			roleMenu = manager
-			break
-		case 'operator':
-			roleMenu = operator
-			break
-		case 'teacher':
-			roleMenu = teacher
-			break
-	}
-} else {
-	// roleMenu = views
-}
+//
+// const userData = JSON.parse(localStorage.getItem('userData'))
+// if (userData && userData.role) {
+// 	switch (userData.role) {
+// 		case 'admin':
+// 			roleMenu = admin
+// 			break
+// 		case 'director':
+// 			roleMenu = director
+// 			break
+// 		case 'manager':
+// 			roleMenu = manager
+// 			break
+// 		case 'operator':
+// 			roleMenu = operator
+// 			break
+// 		case 'teacher':
+// 			roleMenu = teacher
+// 			break
+// 	}
+// } else {
+	roleMenu = views
+// }
 
 export default roleMenu
